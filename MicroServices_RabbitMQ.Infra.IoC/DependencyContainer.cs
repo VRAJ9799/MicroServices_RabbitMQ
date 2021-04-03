938093ae098ed28cd67dc7c0496c6992f64b73f8
VRAJ SHAH <vrajshah363@gmail.com>
2021-04-03 14:30:30 +0530

    added dependency

---
+++ MicroServices_RabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroServices_RabbitMQ.Domain.Core.Bus;
+using MicroServices_RabbitMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroServices_RabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
